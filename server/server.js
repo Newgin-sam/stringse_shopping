fe7a6fcf5148e259914c249bafd974161f3b51aa
Newgin Sam <newginsam@gmail.com>
2021-08-14 09:21:03 +0530

    test

--- server/server.js
+++ server/server.js
@@ -1,69 +1,62 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const xss = require('xss-clean');
 const mongoSanitize = require('express-mongo-sanitize');
-const routes = require('./routes')
+const routes = require('./routes');
 const passport = require('passport');
-
 const { jwtStrategy } = require('./middleware/passport');
-const { handleError, converToApiError } = require('./middleware/apiError')
 
-require('dotenv').config();
+const { handleError, converToApiError } = require('./middleware/apiError');
 
-const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`
 
-mongoose.connect(mongoURI, {
+// mongodb+srv://admin:<password>@cluster0.kbuow.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-})
+});
 
 
 
-// instead of body parser
-app.use(express.json());
+/// body parse
+app.use(express.json())
 
 
-// sanitize the req
+/// sanitize
 app.use(xss());
 app.use(mongoSanitize());
 
-
-//passport
+// passport 
 app.use(passport.initialize());
 passport.use('jwt', jwtStrategy);
 
 /// routes
-app.use('/api', routes);
+app.use('/api', routes)
 
 
-// handle Error
+/// HANDLE ERRORS
 /// if the error not recognized....convert to api error
-
 app.use(converToApiError);
-
 app.use((err, req, res, next) => {
-    handleError(err, res);
+    handleError(err, res)
 })
+
 
 app.use(express.static('client/build'));
-
-app.get('/api/test', (req, res) => {
-    res.send("hello buddy");
-})
-
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
     });
 }
 
 const port = process.env.PORT || 5000
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
-})
+});
