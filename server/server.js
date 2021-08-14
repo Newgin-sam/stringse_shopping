a48ea06e4357f7556ca55cfba2e96d042a2514d5
Newgin Sam <newginsam@gmail.com>
2021-08-14 09:07:12 +0530

    deploy issue fix

--- server/server.js
+++ server/server.js
@@ -55,16 +55,15 @@ app.get('/api/test', (req, res) => {
     res.send("hello buddy");
 })
 
-// if (process.env.NODE_ENV === 'production') {
-const path = require('path');
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-});
-// }
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
-console.log(process.env.NODE_ENV)
+const port = process.env.PORT || 5000
 
-const port = process.env.PORT || 8082
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
